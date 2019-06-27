be904fef6daf8c3f4a5b748d4694f8c8f5cac0d4
Fi1osof <n.lanets@modxclub.ru>
2019-06-27 19:18:15 +0300

    1.0.1

--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+1.0.1
+=================================
+- Remove apiFragments
+- Upgrade @prisma-cms/front
+
 1.0.0
 =================================
 - First release
--- src/dev/App.js
+++ src/dev/App.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import PrismaCmsApp from '@prisma-cms/front'
 import { Renderer as PrismaCmsRenderer } from '@prisma-cms/front'
 
-import * as queryFragments from "@prisma-cms/front/lib/schema/generated/api.fragments";
+// import * as queryFragments from "@prisma-cms/front/lib/schema/generated/api.fragments";
 
 import App, {
   SingleUploader,
@@ -134,23 +134,19 @@ class DevRenderer extends PrismaCmsRenderer {
 export default class DevApp extends Component {
 
   static propTypes = {
-    queryFragments: PropTypes.object.isRequired,
   }
 
   static defaultProps = {
-    queryFragments,
     lang: "ru",
   }
 
   render() {
 
     const {
-      queryFragments,
       ...other
     } = this.props;
 
     return <PrismaCmsApp
-      queryFragments={queryFragments}
       Renderer={DevRenderer}
       // pure={true}
       {...other}
